0d651692fe812f2b35fc6ed0003bd0b2c8b10d33
Jaxck <losofst@gmail.com>
2025-05-28 19:19:10 +0300

    a little decorative changes

--- src/weather/api_requests/weather.py
+++ src/weather/api_requests/weather.py
@@ -5,6 +5,21 @@ from django.core.cache import cache
 
 from config.settings import WEATHER_API_KEY, BASE_URL
 
+months = {
+    '01': 'January',
+    '02': 'February',
+    '03': 'March',
+    '04': 'April',
+    '05': 'May',
+    '06': 'June',
+    '07': 'July',
+    '08': 'August',
+    '09': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December'
+}
+
 
 def parse_current_weather(data: dict, city: str) -> dict:
     '''Функция для получения погоды на данный момент. Не кешируемая
@@ -23,7 +38,7 @@ def parse_current_weather(data: dict, city: str) -> dict:
     result = {
         "city": location.get("name"),
         "icon": data_condition.get('icon'),
-        "text": data_condition.get('text'),
+        "text": data_condition.get('text').lower(),
         "temp": data.get("temp_c"),
     }
     return result
@@ -68,7 +83,6 @@ def get_current_forecast_weather(city: str) -> dict:
     req = requests.get(f"{BASE_URL}/forecast.json?q={city}&days=7&key={WEATHER_API_KEY}")
     req_result = json.loads(req.text)
 
-
     result = {
         "current": {
             **parse_current_weather(req_result, city)
@@ -81,7 +95,8 @@ def get_current_forecast_weather(city: str) -> dict:
         }
     for i in req_result['forecast']["forecastday"]:
         day_info = i['day']
-        day = i['date'][-2:] if i["date"][-2] != '0' else i['date'][-1]
+        month = months[i["date"][-5:-3]]
+        day = f"{i['date'][-2:]} {month}" if i["date"][-2] != '0' else f"{i['date'][-1]} {month}"
         result["forecast"][day] = {
             "text": day_info["condition"]["text"],
             "icon": day_info["condition"]["icon"],
